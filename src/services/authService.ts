ff2d7f0b9035bbd96b8c61ef0fe2ca14617a2ca4
JaviF <efiallos2433@uta.edu.ec>
2025-06-20 17:28:47 -0500

    Se agrego servicio de login

---
+++ src/services/authService.ts
@@ -0,0 +1,14 @@
+import { API_URL_BASE } from '../utils/api';
+
+export async function loginService(email: string, password: string) {
+  const res = await fetch(`${API_URL_BASE}api/auth/login`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ email, password }),
+  });
+  const data = await res.json();
+  if (!res.ok) {
+    throw new Error(data.message || 'Usuario o contraseña incorrectos');
+  }
+  return data;
+} 
